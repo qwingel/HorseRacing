b5a5e682b8d2b0045a3c463a4cb0b6a1206bf1fd
qwingel <mazzi12321@gmail.com>
2025-07-05 12:30:01 +0300

    start

--- app/src/main/java/com/example/kotlinstart/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstart/MainActivity.kt
@@ -2,45 +2,22 @@ package com.example.kotlinstart
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.viewpager2.widget.ViewPager2
-import com.example.kotlinstart.fragments.GameFragment
-import com.example.kotlinstart.fragments.HistoryFragment
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         supportActionBar?.hide()
-        InitViewPager()
-    }
 
-    fun InitViewPager() {
-        // Список фраментов
-        val fragments = listOf(
-            GameFragment(),
-            HistoryFragment()
-        )
 
-        // ViewPager2
-        val viewPager = findViewById<ViewPager2>(R.id.view_pager)
-        viewPager.adapter = ViewPagerAdapter(this, fragments)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        // TabLayout
-        val tabLayout = findViewById<TabLayout>(R.id.tab_bar)
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = when(position) {
-                0 -> "Скачки"
-                1 -> "История"
-                else -> null
-            }
-            tab.icon = when(position) {
-                0 -> ContextCompat.getDrawable(this, R.drawable.horses) // Иконка кони
-                1 -> ContextCompat.getDrawable(this, R.drawable.history) // Иконка история
-                else -> null
-            }
-        }.attach()
+        // BottomNavigation + NavController
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/kotlinstart/fragments/RaceFragment.kt
+++ app/src/main/java/com/example/kotlinstart/fragments/RaceFragment.kt
@@ -1,18 +1,18 @@
 package com.example.kotlinstart.fragments
-import com.example.kotlinstart.R
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.kotlinstart.R
 
-class GameFragment : Fragment() {
+class RaceFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.game_fragment, container, false)
+    ): View {
+        return inflater.inflate(R.layout.race_fragment, container, false)
     }
 }
